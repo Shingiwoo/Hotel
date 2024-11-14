3988b8d825ee6b8c7086ea17c4bdbf732177ee65
shingiwoo <shingiwoo.ind@gmail.com>
2024-11-14 15:57:13 +0700

    "Updated AdminController, added password change functionality, and modified admin dashboard and header views."

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -45,13 +46,67 @@ class AdminController extends Controller
         $data->address = $request->address;
 
         if ($request->file('photo')) {
+            // Hapus file lama jika ada
+            $oldImagePath = public_path('upload/admin_images/' . $data->photo);
+            if (file_exists($oldImagePath)) {
+                unlink($oldImagePath);
+            }
+
+            // Simpan file baru
             $file = $request->file('photo');
             $filename = date('YmdHi') . '.' . $file->getClientOriginalName();
             $file->move(public_path('upload/admin_images'), $filename);
-            $data['photo'] = $filename;
-        }
-        $data->save();
 
-        return redirect()->back();
+            // Update nama file dalam database
+            $data->photo = $filename;
+
+        }$data->save();
+
+        $notification = array(
+            'message' => 'Admin Profile Update Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
     }
+
+    public function AdminChangePassword(){
+
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+
+        return view('admin.admin_change_password', compact('profileData'));
+    }
+
+    public function AdminPasswordUpdate(Request $request){
+
+        //Valiadtion
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed',
+        ]);
+
+        if (!Hash::check($request->old_password, Auth::user()->password)) {
+
+            $notification = array(
+                'message' => 'Old Password Does not Match!',
+                'alert-type' => 'error'
+            );
+
+            return back()->with($notification);
+        }
+
+        // Update The New Password
+        User::whereId(Auth::user()->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+
+        $notification = array(
+            'message' => 'Password Change Successfully',
+            'alert-type' => 'success'
+        );
+
+        return back()->with($notification);
+    }
+
 }
